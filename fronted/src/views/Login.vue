eaa41f22b78708910c093fbfa6bb6692d85a9f43
obf1313 <oubaifan@163.com>
2019-01-17 21:49:08 +0800

    login register

---
+++ fronted/src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <Row>
+        <Row class="header" type="flex" justify="end">
+            <router-link :to="{ name: 'Register'}"><Button type="text" style="color: #fff">注册</Button></router-link>
+        </Row>
+        <Row type="flex" justify="center" style="margin-top: 200px;">
+            <Row style="width: 300px;">
+                <Row class="title">登录</Row>
+                <Input placeholder="请输入用户名" v-model="userName" clearable />
+                <Input placeholder="请输入密码" v-model="passWord" clearable />
+                <Button type="success" style="width: 100%">登陆</Button>
+            </Row>
+        </Row>
+    </Row>
+</template>
+
+<script>
+    import { Button, Row, Input} from 'element-ui'
+    export default {
+        name: "Login",
+        mounted: ()=>{
+        },
+        data() {
+            return {
+                userName: '',
+                passWord: ''
+            }
+        },
+        components: {
+            Button,
+            Row,
+            Input
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    @import "../assets/css/login.css";
+</style>
---
+++ fronted/src/views/Register.vue
@@ -0,0 +1,42 @@
+<template>
+    <Row>
+        <Row class="header" type="flex" justify="end">
+            <router-link :to="{ name: 'Login'}"><Button type="text" style="color: #fff">登录</Button></router-link>
+        </Row>
+        <Row type="flex" justify="center" style="margin-top: 200px;">
+            <Row style="width: 300px;">
+                <Row class="title">注册</Row>
+                <Input placeholder="请输入用户名" v-model="userName" clearable />
+                <Input placeholder="请输入密码" v-model="passWord" clearable />
+                <Button type="success" style="width: 100%">登录</Button>
+            </Row>
+        </Row>
+    </Row>
+</template>
+
+<script>
+    import { Button, Row, Input } from 'element-ui'
+    export default {
+        name: "Register",
+        mounted: ()=>{
+        },
+        data() {
+            return {
+                userName: '',
+                passWord: ''
+            }
+        },
+        components: {
+            Button,
+            Row,
+            Input
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    @import "../assets/css/login.css";
+</style>
