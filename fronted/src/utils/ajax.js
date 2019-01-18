8854fe2ee166c175d394e4bac56a64ddf8af2c52
obf1313 <oubaifan@163.com>
2019-01-18 17:33:32 +0800

    json

--- fronted/src/utils/ajax.js
+++ fronted/src/utils/ajax.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 let Ajax = {
     post: function (url, data={}) {
-        return axios.post('http://127.0.0.1:8000/messageBox'+url,data).then(function(response) {
+        return axios.post('http://127.0.0.1:8000/messageBox'+url, JSON.stringify(data)).then(function(response) {
             window.console.log(response)
         }).catch((error)=>{
             window.console.log(error)
         })
     },
     get: function (url, data={}) {
-        return axios.get('http://127.0.0.1:8000/messageBox'+url,data).then(function(response) {
+        return axios.get('http://127.0.0.1:8000/messageBox'+url, data).then(function(response) {
             window.console.log(response)
         }).catch((error)=>{
             window.console.log(error)
